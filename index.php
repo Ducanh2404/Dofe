b703ec3e0c43e6a1eda957bde9c0a13716661533
Ducanh2404 <1951060533@e.tlu.edu.vn>
2023-08-18 16:15:49 +0700

    new

--- index.php
+++ index.php
@@ -5,8 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./img/logo.svg" type="image/x-icon">
     <title>Dofe Cafe</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-    
+    <div class="container-fluid navbar">
+        <div class="row">
+            <div class="col">
+                <ul class="navbar-items">
+                    <li><a href="">Trang Chủ</a></li>
+                    <li><a href="">Menu</a></li>
+                    <li><a href="">Liên hệ</a> </li>
+                </ul>
+                <ul class="navbar-logo">
+                    
+                </ul>
+            </div>
+        </div>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
