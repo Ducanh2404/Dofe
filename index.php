cd3ee7537cf2b7046302017db49a86c9bc0fcb98
Ducanh2404 <1951060533@e.tlu.edu.vn>
2023-08-22 17:07:27 +0700

    new

--- index.php
+++ index.php
@@ -6,26 +6,24 @@
     <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
     <title>Dofe Cafe</title>
     <link rel="stylesheet" href="css/index.css">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 <body>
     <div class="container-fluid navbar">
         <div class="row">
-            <div class="col-sm-3 nav-left">
-                <ul class="d-flex">
-                    <li><a href="">Trang Chủ</a></li>
-                    <li><a href="">Menu</a></li>
-                    <li><a href="">Liên hệ</a> </li>
+            <div class="col-sm-12">
+                <ul class="nav-items">
+                    <li class="active"><a href="">Trang Chủ</a></li>
+                    <li><a href="">Thực Đơn</a></li>
+                    <img src="img/logo1.jpg" width="5%" height="5%" style="margin:0 5px!important">
+                    <li><a href="">Về Dofé</a> </li>
+                    <li><a href="">Tuyển Dụng</a> </li>
                 </ul>
-                </div>
-
-                <div class="col-sm-3">
-                <div class="navbar-logo">
-                    <a href=""><img src="img/logo.png"  ></a>
-                </div>
             </div>
-            <div class="col-sm-3"></div>
         </div>
+    </div>
+    <div class="container-fluid">
+        <img src="img/container.jpg" alt="" height="80%">
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
