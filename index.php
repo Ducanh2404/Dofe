4943703618f3f49869cb5cb3cfa103f73da6fe5e
Ducanh2404 <1951060533@e.tlu.edu.vn>
2023-08-23 07:22:48 +0700

    new

--- index.php
+++ index.php
@@ -9,13 +9,16 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container-fluid navbar">
+    <div class="container-fluid navbar d-flex justify-content-center">
         <div class="row">
+            <div class="col-12 d-none">
+                <img src="img/279100714_297374525893521_1478490873479549619_n.jpg" alt="">
+            </div>
             <div class="col-sm-12">
                 <ul class="nav-items">
                     <li class="active"><a href="">Trang Chủ</a></li>
                     <li><a href="">Thực Đơn</a></li>
-                    <img src="img/logo1.jpg" width="5%" height="5%" style="margin:0 5px!important">
+                    <a href="index.php" ><img src="img/logo1.jpg" width="90px" height="90px" style="margin:0 5px!important"></a>
                     <li><a href="">Về Dofé</a> </li>
                     <li><a href="">Tuyển Dụng</a> </li>
                 </ul>
@@ -23,7 +26,7 @@
         </div>
     </div>
     <div class="container-fluid">
-        <img src="img/container.jpg" alt="" height="80%">
+        <img src="img/container.jpg" width="100%" height="90%" alt="">
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
